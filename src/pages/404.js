f2f154253f53a123851690b8a66c5296acb0e86b
yin01 <yin01@uchicago.edu>
2020-08-15 12:01:56 -0500

    Edit

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,19 @@
 import React from "react"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+import PageContainer from "../components/pagecontainer"
+ 
+class NotFoundPage extends PageContainer {
+  render(props){
+    return(
+    <Layout isMobile={this.state.isMobile} 
+            pagewidth={this.state.width}>
+        <SEO title="404: Not found" />
+        <h1>NOT FOUND</h1>
+        <p>You just hit a route that doesn&#39;t exist...</p>
+    </Layout>
+    )
+  }
+}
 
 export default NotFoundPage
